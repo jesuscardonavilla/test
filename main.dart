cdc13d1c0bf2431513188fd1f48af667015babb0
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-04-29 16:05:09 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,27 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:languify/screens/language_screen.dart';
 
+import 'level_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _language = 'English';
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Languify',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LanguageScreen(language: _language),
+      title: 'Short Stories',
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Short Stories')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LevelPage(language: 'English'),
+                ),
+              );
+              // TODO: Add code to navigate to the LevelPage for English
+            },
+            child: Text('English'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LevelPage(language: 'Italiano'),
+                ),
+              );
+
+              // TODO: Add code to navigate to the LevelPage for Italiano
+            },
+            child: Text('Italiano'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LevelPage(language: '中文'),
+                ),
+              );
+              // TODO: Add code to navigate to the LevelPage for 中文
+            },
+            child: Text('中文'),
+          ),
+        ],
+      ),
+    );
+      // TODO: Add your widget tree for the HomePage here
   }
 }
