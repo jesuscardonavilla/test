25bb89a8dc96aa7d1c93a18f82b777e4a9795a2b
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-04-29 16:05:56 -0400

    Update dificultypage_screen.dart

--- dificultypage_screen.dart
+++ dificultypage_screen.dart
@@ -1,42 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:lingofy_again/story_page.dart';
 
-class DifficultyPageScreen extends StatelessWidget {
-  final String title;
-  final List<String> difficulties;
-  final Function(String) onDifficultySelected;
+class LevelPage extends StatefulWidget {
+  final String language;
 
-  DifficultyPageScreen({
-    required this.title,
-    required this.difficulties,
-    required this.onDifficultySelected,
-  });
+  const LevelPage({Key? key, required this.language}) : super(key: key);
 
-  Widget _buildDifficultyButton(String difficulty) {
-    return ElevatedButton(
-      onPressed: () => onDifficultySelected(difficulty),
-      child: Text(difficulty),
-    );
-  }
+  @override
+  _LevelPageState createState() => _LevelPageState();
+}
 
-  Widget _buildDifficultyList() {
-    return Column(
-      children: difficulties.map((difficulty) => _buildDifficultyButton(difficulty)).toList(),
-    );
-  }
-
-  Widget difficultyScreen(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: _buildDifficultyList(),
-      ),
-    );
-  }
+class _LevelPageState extends State<LevelPage> {
+  final Map<String, Map<String, String>> levelLabels = {
+    'English': {
+      'Beginner': 'Beginner',
+      'Intermediate': 'Intermediate',
+      'Advanced': 'Advanced',
+    },
+    'Italiano': {
+      'Beginner': 'Principiante',
+      'Intermediate': 'Intermedio',
+      'Advanced': 'Avanzato',
+    },
+    '中文': {
+      'Beginner': '初学者',
+      'Intermediate': '中级',
+      'Advanced': '高级',
+    },
+  };
 
   @override
   Widget build(BuildContext context) {
-    return difficultyScreen(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('${widget.language} Levels')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StoryPage(
+                    language: widget.language,
+                    level: 'Beginner',
+                  ),
+                ),
+              );
+
+              // TODO: Add code to navigate to the StoryPage for the beginner level
+            },
+            child: Text(levelLabels[widget.language]!['Beginner']!),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StoryPage(
+                    language: widget.language,
+                    level: 'Intermediate',
+                  ),
+                ),
+              );
+
+              // TODO: Add code to navigate to the StoryPage for the intermediate level
+            },
+            child: Text(levelLabels[widget.language]!['Intermediate']!),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StoryPage(
+                    language: widget.language,
+                    level: 'Advanced',
+                  ),
+                ),
+              );
+
+              // TODO: Add code to navigate to the StoryPage for the advanced level
+            },
+            child: Text(levelLabels[widget.language]!['Advanced']!)
+            ,
+          ),
+        ],
+      ),
+    );
   }
 }
